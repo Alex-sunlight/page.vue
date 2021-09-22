10269e8bc3fc740f90cf81aa6e5d635a9ed2a8a1
Alex-sunlight <yp.sunlight@gmail.com>
2021-09-22 21:26:54 +0800

    制作播放器

---
+++ pages/audio/audio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a href="./audio.css"></a>
+  </div>
+</template>
+<script>
+export default {
+  head: {
+    script: [
+      {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js',
+      },
+    ],
+  },
+}
+</script>
+<style lang="stylus"></style>
